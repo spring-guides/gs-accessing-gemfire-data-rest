2ff6b76f454af1dbe805ed8a106d5c775350e2e4
John Blum <jblum@pivotal.io>
2017-11-12 22:05:52 -0800

    Replace all Spring Java-based configuration meta-data with Spring Data GemFire Annotation-based configuration to configure Pivotal GemFire.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,51 +1,34 @@
 
 package hello;
 
-import java.util.Properties;
-
+import org.apache.geode.cache.GemFireCache;
+import org.apache.geode.cache.client.ClientRegionShortcut;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.gemfire.CacheFactoryBean;
-import org.springframework.data.gemfire.LocalRegionFactoryBean;
+import org.springframework.data.gemfire.client.ClientRegionFactoryBean;
+import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
 import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
 
-import com.gemstone.gemfire.cache.GemFireCache;
-
 @SpringBootApplication
+@ClientCacheApplication(name = "DataGemFireRestApplication", logLevel = "error")
 @EnableGemfireRepositories
 @SuppressWarnings("unused")
 public class Application {
-
-	@Bean
-	Properties gemfireProperties() {
-		Properties gemfireProperties = new Properties();
-		gemfireProperties.setProperty("name", "DataGemFireRestApplication");
-		gemfireProperties.setProperty("mcast-port", "0");
-		gemfireProperties.setProperty("log-level", "config");
-		return gemfireProperties;
-	}
-
-	@Bean
-	CacheFactoryBean gemfireCache() {
-		CacheFactoryBean gemfireCache = new CacheFactoryBean();
-		gemfireCache.setClose(true);
-		gemfireCache.setProperties(gemfireProperties());
-		return gemfireCache;
-	}
-
-	@Bean
-	LocalRegionFactoryBean<String, Person> helloRegion(GemFireCache cache) {
-		LocalRegionFactoryBean<String, Person> helloRegion = new LocalRegionFactoryBean<>();
-		helloRegion.setCache(cache);
-		helloRegion.setClose(false);
-		helloRegion.setName("hello");
-		helloRegion.setPersistent(false);
-		return helloRegion;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean("People")
+	public ClientRegionFactoryBean<Object, Object> peopleRegion(GemFireCache gemfireCache) {
+
+		ClientRegionFactoryBean<Object, Object> peopleRegion = new ClientRegionFactoryBean<>();
+
+		peopleRegion.setCache(gemfireCache);
+		peopleRegion.setClose(false);
+		peopleRegion.setShortcut(ClientRegionShortcut.LOCAL);
+
+		return peopleRegion;
+	}
 }
