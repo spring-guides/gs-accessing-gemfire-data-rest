c5d1aa9b69d292212cea98af0aba064369e5589b
John Blum <jblum@pivotal.io>
2018-03-01 13:10:10 -0800

    Fix sample code and Maven/Gradle build files to run with Spring Boot 2.0.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,34 +1,22 @@
 
 package hello;
 
-import org.apache.geode.cache.GemFireCache;
 import org.apache.geode.cache.client.ClientRegionShortcut;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.gemfire.client.ClientRegionFactoryBean;
 import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
+import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
 import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
 
 @SpringBootApplication
-@ClientCacheApplication(name = "DataGemFireRestApplication", logLevel = "error")
+@ClientCacheApplication(name = "AccessingGemFireDataRestApplication", logLevel = "error")
+@EnableEntityDefinedRegions(basePackageClasses = Person.class,
+	clientRegionShortcut = ClientRegionShortcut.LOCAL)
 @EnableGemfireRepositories
 @SuppressWarnings("unused")
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean("People")
-	public ClientRegionFactoryBean<Object, Object> peopleRegion(GemFireCache gemfireCache) {
-
-		ClientRegionFactoryBean<Object, Object> peopleRegion = new ClientRegionFactoryBean<>();
-
-		peopleRegion.setCache(gemfireCache);
-		peopleRegion.setClose(false);
-		peopleRegion.setShortcut(ClientRegionShortcut.LOCAL);
-
-		return peopleRegion;
 	}
 }
