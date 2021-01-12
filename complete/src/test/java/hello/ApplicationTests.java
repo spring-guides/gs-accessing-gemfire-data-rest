3a2fa1c9209c413b530ed14b4ffcce629837c988
John Blum <jblum@pivotal.io>
2021-01-11 17:34:39 -0800

    Refactor tests to use JUnit 5.

--- complete/src/test/java/hello/ApplicationTests.java
+++ complete/src/test/java/hello/ApplicationTests.java
@@ -27,19 +27,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@SuppressWarnings("unused")
 public class ApplicationTests {
 
 	@Autowired
@@ -48,8 +50,8 @@ public class ApplicationTests {
 	@Autowired
 	private PersonRepository personRepository;
 
-	@Before
-	public void deleteAllBeforeTests() throws Exception {
+	@BeforeEach
+	public void deleteAllBeforeTests() {
 		personRepository.deleteAll();
 	}
 
